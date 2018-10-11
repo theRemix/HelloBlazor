10ad1a50f71021ba1669e89ad901245390758c56
Jon Borgonia <jon@gomagames.com>
2018-10-10 18:59:30 -0700

    Add vanilla "components" for comparison
    - add interop to invoke vanilla js functions
    - add firecharts and options to render 100 and 10000 items

---
+++ HelloBlazor/Shared/VanillaInterop.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Blazor;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace HelloBlazor
+{
+    public class VanillaInterop
+    {
+        public static Task<string> RenderListOfComponentsJS(int num)
+        {
+            // Implemented in js/list-of-components.js
+            return JSRuntime.Current.InvokeAsync<string>(
+                "renderListOfComponentsJS",
+                num);
+        }
+    }
+}
